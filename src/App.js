bf2430431241846ea54ee5ab1a77cd3c65d26ba9
kydsib <simonas.kersulis@gmail.com>
2019-09-03 17:13:16 +0300

    Added Header and SignIn form

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
+import Header from "./componnets/header/header.component";
+import SignInSignUpPage from "./pages/sign-in--sign-up/sign-in--sign-up.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUpPage} />
       </Switch>
     </div>
   );
