dbabe7296be629cd14589189bd41e25c27bdad95
kydsib <simonas.kersulis@gmail.com>
2019-10-31 18:02:37 +0200

    added cart item functionality, updated shop data

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,11 @@
+import CartActionTypes from './cart.types'
+
+export const toggleCartHidden = () => ({
+	type: CartActionTypes.TOGGLE_CART_HIDDEN
+})
+
+export const addItem = item => ({
+	// function that gets item and generate new action w/ type and payload
+	type: CartActionTypes.ADD_ITEM,
+	payload: item
+})
