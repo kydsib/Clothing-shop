8b4d4011ad50cb36cb4f3735bd9db3e9e8c33917
kydsib <simonas.kersulis@gmail.com>
2020-02-18 17:36:11 +0200

    add and remove item functionality added to checkout-item component

--- src/componnets/checkout-item/checkout-item.component.jsx
+++ src/componnets/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import {
+	clearItemFromCart,
+	addItem,
+	reduceItemByOne
+} from '../../redux/cart/cart.actions'
 
 import './checkout-item.styles.scss'
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-	<div className="checkout-item">
-		<div className="image-container">
-			<img src={imageUrl} alt="item" />
+const CheckoutItem = ({
+	// cartItem: { name, imageUrl, price, quantity }, cant use it like this, because I wont have acces to cartItem
+	cartItem,
+	deleteItem,
+	addOne,
+	removeItem
+}) => {
+	const { name, imageUrl, price, quantity } = cartItem
+	return (
+		<div className="checkout-item">
+			<div className="image-container">
+				<img src={imageUrl} alt="item" />
+			</div>
+			<span className="name">{name}</span>
+			<span className="quantity">
+				<div className="arrow" onClick={() => removeItem(cartItem)}>
+					&#10094;
+				</div>
+				<span className="value">{quantity}</span>
+				<div className="arrow" onClick={() => addOne(cartItem)}>
+					&#10095;
+				</div>
+			</span>
+			<span className="price">{price}</span>
+			<div className="remove-button" onClick={() => deleteItem(cartItem)}>
+				&#10005;
+			</div>
 		</div>
-		<span className="name">{name}</span>
-		<span className="quantity">{quantity}</span>
-		<span className="price">{price}</span>
-		<div className="remove-button">&#10005;</div>
-	</div>
-)
+	)
+}
 
-export default CheckoutItem
+const mapDispatchToProps = dispatch => ({
+	deleteItem: item => dispatch(clearItemFromCart(item)),
+	addOne: item => dispatch(addItem(item)),
+	removeItem: item => dispatch(reduceItemByOne(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem)
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,3 +14,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 	return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 	// quantity get attached to the first new item, thats why we can call it in if statement
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+	const existingCartItem = cartItems.find(
+		cartItem => cartItem.id === cartItemToRemove.id
+	)
+
+	if (existingCartItem.quantity === 1) {
+		return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+	}
+
+	return cartItems.map(cartItem =>
+		cartItem.id === cartItemToRemove.id
+			? { ...cartItem, quantity: cartItem.quantity - 1 }
+			: cartItem
+	)
+}
