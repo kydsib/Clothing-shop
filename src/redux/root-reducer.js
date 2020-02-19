09cc3e128744b7a80a8fab9dda6891a7de166c06
kydsib <simonas.kersulis@gmail.com>
2020-02-19 22:09:14 +0200

    added ability to store items in localStorage using redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // actual loacalStorage object, redux persist is using LS as default storage
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
 
-export default combineReducers({
+const persistConfig = {
+	key: 'root', // telling at witch point of reducer to start storing values
+	storage,
+	whitelist: ['cart'] // only persisting cart because user is handled by firebase
+}
+
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
