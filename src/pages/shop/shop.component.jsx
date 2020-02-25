15c57519fe98faf6868ce7eeff17ceee7190d4fd
kydsib <simonas.kersulis@gmail.com>
2020-02-25 12:15:50 +0200

    directory and shop data moved to it's own reducers

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import CollectionOverview from '../../componnets/collections-overview/collections-overview.component'
+import CollectionPage from '../collection/collection.component'
 
-const ShopPage = () => (
-	<div className="shop-page">
-		<CollectionOverview />
-	</div>
-)
+const ShopPage = ({ match }) => {
+	return (
+		// shop in app component is nested in Route so we can use match
+		<div className="shop-page">
+			<Route exact path={match.path} component={CollectionOverview} />
+			<Route
+				path={`${match.path}/:collectionId`}
+				component={CollectionPage}
+			/>
+		</div>
+	)
+}
 
 export default ShopPage
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,15 @@ export const selectCollections = createSelector(
 	[selectShop],
 	shop => shop.collections
 )
+
+// changed shop data from arr to obj. Collection-overview uses arr so we convert obj to arr and using it in collections-overview
+export const selectCollectionsForPreview = createSelector(
+	[selectCollections],
+	collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam =>
+	createSelector(
+		[selectCollections],
+		collections => collections[collectionUrlParam]
+	)
